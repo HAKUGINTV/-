1d672b94a04616ba53289024f086b8cc64b5d096
EC2 Default User <158741872+HAKUGINTV@users.noreply.github.com>
2024-02-26 12:50:17 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # get 'books/new'
   # post 'books' => 'books#create'
   # get 'books/index'
-root 'books#index'
+# root 'books#index'
   # get 'books/show'
   # get 'books/edit'
   get 'top', to: 'homes#top'
   # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   # patch 'books/:id' => 'books#update', as: 'update_book'
   # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-root 'books#index'
+root 'homes#top'
 
   resources :books
 end
